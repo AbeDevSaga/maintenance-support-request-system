bc5db17ddd7817af80bc11b32f38bd7babf3c3f6
esuba-sol <esuba6564@gmail.com>
2026-01-02 07:04:36 +0000

    updated branch

--- frontend/src/utils/fileUrl.ts
+++ frontend/src/utils/fileUrl.ts
@@ -2,11 +2,11 @@ export const getFileUrl = (filePath: string) => {
   // const apiBase =
   //   process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4000/api";
 
-  const apiBase = import.meta.env.VITE_API_PUBLIC_BASE_URL;
+  const apiBase = import.meta.env.VITE_API_SERVER_BASE_URL || "http://http://196.188.240.103:4038";
 
-  const serverBase = apiBase.replace(/\/api\/?$/, "");
+  // const serverBase = apiBase.replace(/\/api\/?$/, "");
 
-  return `${serverBase}/${filePath.replace(/\\/g, "/")}`;
+  return `${apiBase}/${filePath.replace(/\\/g, "/")}`;
 };
 
 export const getFileType = (fileName: string): string => {
